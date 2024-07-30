5de82cc9b42e9e0e302261691945e290c62de882
Karolina Piotrowska <kapiotrow@student.agh.edu.pl>
2024-07-30 19:37:46 +0200

    added some useful files

--- python_model/README.md
+++ python_model/README.md
@@ -1,1 +1,10 @@
 # MOSSE Object Tracker Python Model
+
+This is the Python model used to evaluate the basic behaviour of the MOSSE object tracker to be implemented in FPGA. 
+
+## How to use
+````
+python test.py --video_path ./videos/car.mp4
+````
+
+## References
---
+++ python_model/test.py
@@ -0,0 +1,65 @@
+import argparse
+import os
+import sys
+import time
+
+import cv2
+import numpy as np
+
+_CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(_CURRENT_DIR, "../"))
+if True:  # noqa F402
+    from mosse_tracker import Mosse
+
+
+def get_args():
+    parser = argparse.ArgumentParser("")
+    parser.add_argument("--video_path", type=str, required=True)
+
+    return parser.parse_args()
+
+
+def main():
+    args = get_args()
+
+    cap = cv2.VideoCapture(args.video_path)
+    shape = (int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+    fps = cap.get(cv2.CAP_PROP_FPS)
+
+    ok, frame = cap.read()
+    if not ok:
+        print(f"cannot read: {args.video_path}")
+        sys.exit()
+
+    tracker = Mosse(frame_shape=shape)
+
+    # bbox = cv2.selectROI(frame, False)
+    bbox = (425, 302, 66, 43)
+
+    if not tracker.init(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY), bbox):
+        print("failed to initialize tracking")
+        sys.exit()
+
+    while cap.isOpened():
+        ok, frame = cap.read()
+        if not ok:
+            break
+
+        frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        output_bbox = tracker.update(frame_gray)
+
+        if not output_bbox:
+            print("failed to track")
+            break
+
+        x_min, y_min, w, h = output_bbox
+        cv2.rectangle(frame, (x_min, y_min), (x_min + w, y_min + h), (255, 0, 0), 2, 1)
+        cv2.imshow("tracked result", frame)
+
+        k = cv2.waitKey(int(1000 / fps)) & 0xFF
+        if k == 27:
+            break
+
+
+if __name__ == "__main__":
+    main()
